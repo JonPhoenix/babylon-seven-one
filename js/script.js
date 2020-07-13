d7db6f3466b42c3e55cce28b435cb4d8131d3dda
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-12 17:53:24 -0700

    Adding files

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Babylon Day Planner script
+// Variables declaration
+let myVariable
+
+// if / else if / else / conditionals
+if (condition) {
+    
+}
+else if (condition) {
+    
+} else {
+    
+}
+
+// for loop template
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+// function template
+function name(params) {
+    
+}
