cab8dceb240088e350481fa708fe101729861a89
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-13 13:03:03 -0700

    Updating planner rows

--- js/script.js
+++ js/script.js
@@ -1,56 +1,62 @@
 // Babylon Day Planner jQuery script
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format('HH');
+var currentHourInt = parseInt(currentHour);
 
-$(document).ready(function() {
+// Planner rows data attributes / one row per hour in a day:
+$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+$(document).ready(function () {
+
+    renderPlans();
 
     // Function: Date and time in Header
     $('#currentDay').append();
 
     function currentDate() { 
-        $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm a'));
+        $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
     } setInterval(currentDate, 1000);
 
-    // Planner rows data attributes / one row per hour in a day:
-    $('#9AM-row').attr('data-time', moment('9:00 am', 'h:mm a').format('HH'));
-    $('#10AM-row').attr('data-time', moment('10:00 am', 'h:mm a').format('HH'));
-    $('#11AM-row').attr('data-time', moment('11:00 am', 'h:mm a').format('HH'));
-    $('#12PM-row').attr('data-time', moment('12:00 pm', 'h:mm a').format('HH'));
-    $('#1PM-row').attr('data-time', moment('1:00 pm', 'h:mm a').format('HH'));
-    $('#2AM-row').attr('data-time', moment('2:00 pm', 'h:mm a').format('HH'));
-    $('#3AM-row').attr('data-time', moment('3:00 pm', 'h:mm a').format('HH'));
-    $('#4AM-row').attr('data-time', moment('4:00 pm', 'h:mm a').format('HH'));
-    $('#5AM-row').attr('data-time', moment('5:00 pm', 'h:mm a').format('HH'));
+
 
     // For loop with conditionals if for color change per the hour:
-    var currentHour = moment().format('HH');
-    var currentHourInteger = parseInt(currentHour);
-    var inputHour = $('#' + i + 'row').attr('data-time');
-    var inputHourInteger = parseInt(inputHour);
-
+    
     for (var i = 0; i <= 12; i++) {
-        if (currentHourInteger === inputHourInteger) {
-            $('#' + i + 'row').addClass('past');
+
+        var inputHour = $("#" + i + "Row").attr("data-time");
+        var inputHourInt = parseInt(inputHour);
+
+        if (currentHourInt === inputHourInt) {
+            $("#" + i + "Row").addClass("present");
         }
-        if (currentHourInteger === inputHourInteger) {
-            $('#' + i + 'row').addClass('present');
+        if (currentHourInt > inputHourInt) {
+            $("#" + i + "Row").addClass("past");
         }
-        if (currentHourInteger === inputHourInteger) {
-            $('#' + i + 'row').addClass('future');
+        if (currentHourInt < inputHourInt) {
+            $("#" + i + "Row").addClass("future");
         }
     }
     // Function: Save data button to localStorage on click:
-    var saveButton = $('.saveBtn');
 
-    saveButton.on('click', function () {
-        var oneHour = $(this).attr('this-hour');
-        var userInput = $('#' + oneHour + 'row').val();
-        localStorage.setItem(oneHour, userInput);
+
+    saveBtn.on("click", function () {
+
+        var rowHour = $(this).attr("data-hour");
+        var input = $("#" + rowHour + "Row").val();
+        localStorage.setItem(rowHour, input);
     });
 
-    function savePlans() {
+    function renderPlans() {
         for (var i = 0; i <= 12; i++) {
-            $('#' + i + 'row').val(localStorage.getItem(i));
+            $('#' + i + "Row").val(localStorage.getItem(i));
         }
     }
-
-    savePlans();
 }); 
