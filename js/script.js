b1cccc6b769e6f042d2ba10e3277b0fa048dbfe0
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-13 03:17:26 -0700

    Updating script

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ $(document).ready(function() {
     } setInterval(currentDate, 1000);
 
     // Planner rows data attributes / one row per hour in a day:
-    $('9AM-row').attr('data-time', moment('9:00 am', 'h:mm a').format('HH'));
-    $('10AM-row').attr('data-time', moment('10:00 am', 'h:mm a').format('HH'));
-    $('11AM-row').attr('data-time', moment('11:00 am', 'h:mm a').format('HH'));
-    $('12PM-row').attr('data-time', moment('12:00 pm', 'h:mm a').format('HH'));
-    $('1PM-row').attr('data-time', moment('1:00 pm', 'h:mm a').format('HH'));
-    $('2AM-row').attr('data-time', moment('2:00 pm', 'h:mm a').format('HH'));
-    $('3AM-row').attr('data-time', moment('3:00 pm', 'h:mm a').format('HH'));
-    $('4AM-row').attr('data-time', moment('4:00 pm', 'h:mm a').format('HH'));
-    $('5AM-row').attr('data-time', moment('5:00 pm', 'h:mm a').format('HH'));
+    $('#9AM-row').attr('data-time', moment('9:00 am', 'h:mm a').format('HH'));
+    $('#10AM-row').attr('data-time', moment('10:00 am', 'h:mm a').format('HH'));
+    $('#11AM-row').attr('data-time', moment('11:00 am', 'h:mm a').format('HH'));
+    $('#12PM-row').attr('data-time', moment('12:00 pm', 'h:mm a').format('HH'));
+    $('#1PM-row').attr('data-time', moment('1:00 pm', 'h:mm a').format('HH'));
+    $('#2AM-row').attr('data-time', moment('2:00 pm', 'h:mm a').format('HH'));
+    $('#3AM-row').attr('data-time', moment('3:00 pm', 'h:mm a').format('HH'));
+    $('#4AM-row').attr('data-time', moment('4:00 pm', 'h:mm a').format('HH'));
+    $('#5AM-row').attr('data-time', moment('5:00 pm', 'h:mm a').format('HH'));
 
     // For loop with conditionals if for color change per the hour:
     var currentHour = moment().format('HH');
@@ -37,4 +37,20 @@ $(document).ready(function() {
             $('#' + i + 'row').addClass('future');
         }
     }
-});
+    // Function: Save data button to localStorage on click:
+    var saveButton = $('.saveBtn');
+
+    saveButton.on('click', function () {
+        var oneHour = $(this).attr('this-hour');
+        var userInput = $('#' + oneHour + 'row').val();
+        localStorage.setItem(oneHour, userInput);
+    });
+
+    function savePlans() {
+        for (var i = 0; i <= 12; i++) {
+            $('#' + i + 'row').val(localStorage.getItem(i));
+        }
+    }
+
+    savePlans();
+}); 
