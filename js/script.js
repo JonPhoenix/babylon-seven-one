3a9c28b24900b694f39fe6511066ebe010cf22c2
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-13 17:45:14 -0700

    Final proof reading

--- js/script.js
+++ js/script.js
@@ -10,16 +10,16 @@ $(document).ready(function () {
 
     // Planner rows data attributes / one row per hour in a day:
     $('#9Row').attr('data-time', moment('9:00 am', 'h:mm a').format('HH'));
-    $('#10Row').attr('data-time', moment('10:00 am', 'hh:mm a').format('HH'));
-    $('#11Row').attr('data-time', moment('11:00 am', 'hh:mm a').format('HH'));
-    $('#12Row').attr('data-time', moment('12:00 pm', 'hh:mm a').format('HH'));
+    $('#10Row').attr('data-time', moment('10:00 am', 'h:mm a').format('HH'));
+    $('#11Row').attr('data-time', moment('11:00 am', 'h:mm a').format('HH'));
+    $('#12Row').attr('data-time', moment('12:00 pm', 'h:mm a').format('HH'));
     $('#1Row').attr('data-time', moment('1:00 pm', 'h:mm a').format('HH'));
     $('#2Row').attr('data-time', moment('2:00 pm', 'h:mm a').format('HH'));
     $('#3Row').attr('data-time', moment('3:00 pm', 'h:mm a').format('HH'));
     $('#4Row').attr('data-time', moment('4:00 pm', 'h:mm a').format('HH'));
     $('#5Row').attr('data-time', moment('5:00 pm', 'h:mm a').format('HH'));
 
-    // For loop with conditionals if for color change per the hour:
+    // 'For' loop with conditionals 'if' to change hours colors:
     for (var i = 0; i <= 12; i++) {
         var currentHour = moment().format('HH');
         var currentHourInteger = parseInt(currentHour);
@@ -45,7 +45,7 @@ $(document).ready(function () {
         var userInput = $("#" + oneHour + 'Row').val();
         localStorage.setItem(oneHour, userInput);
     });
-
+    // Function: Keeping data in localStorage after refreshing:
     function savePlanner() {
         for (var i = 0; i <= 12; i++) {
             $('#' + i + 'Row').val(localStorage.getItem(i));
